04e91246ac75b218cd6f9913768f8929e13cfe4e
Feramdark_0898 <70911659+Feramdark@users.noreply.github.com>
2020-11-11 22:53:54 -0600

    Resultados

---
+++ WCFejemplo/IService1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFejemplo
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        //Aqui es donde se declaran los metodos con el contrato de operacion
+        //SI NO SE DECLARAN CON OPERATION CONTRACT NO APARECERAN EN LA VENTANA DE CLIENTE PRUEBA.
+
+        [OperationContract]
+        string GetNumeroControl(int value);//Metodo uno
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);//Metodo ejemplo
+
+        [OperationContract]
+        Alumno GetAlumno(Alumno alumno);//Metodo dos
+        [OperationContract]
+        int calificaciones(int Parcial1, int parcial2, int parcial3);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue=value; }
+        }
+    }
+
+    [DataContract]
+    public class Alumno
+    {
+        bool bandera = true;
+        int nc = 1997;
+        string nombre = "Melisa";
+        string carrera = "Informatica";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return bandera; }
+            set { bandera = value; }
+        }
+
+        [DataMember]
+        public int NoControl
+        {
+            get { return nc; }
+            set { nc = value; }
+        }
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        [DataMember]
+        public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+    }
+}
---
+++ WCFejemplo/Service1.svc.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFejemplo
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        string nulo = "";
+        Alumno alumno = new Alumno();
+        public string GetNumeroControl(int value) //Metodo 1
+        {
+            if (value==1703)
+            {
+                alumno.Nombre = "Fernando Vazquez";
+                return string.Format("El nombre es "+ alumno.Nombre);    
+            }
+            else if (value==1999)
+            {
+                alumno.Nombre = "Aminadad Sandoval";
+                return string.Format("El nombre es " + alumno.Nombre);
+            }
+            else if (value==2010)
+            {
+                alumno.Nombre= "Luis Vazquez";
+                return string.Format("El nombre es " + alumno.Nombre);
+            }
+            return nulo;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)//Deje el metodo de ejemplo
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += ".";
+            }
+            return composite;
+        }
+        public Alumno GetAlumno( Alumno alumno) //Metodo 2
+        {
+            if (alumno.BoolValue)
+            {
+                alumno.NoControl += 0;
+                alumno.Nombre += "";
+                alumno.Carrera +="";
+            }
+            return alumno;
+        }
+        public int calificaciones(int Parcial1, int parcial2,int parcial3)//Metodo 3
+        {
+            int resul = 0;
+            int Semestre = 0;
+            resul = Parcial1 + parcial2 + parcial3;
+            Semestre = resul / 3;
+            return Semestre;
+        }
+    }
+}
